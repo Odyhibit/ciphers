2c8ea0c3a0f33df68af3156a80e8771576484841
Odyhibit <josh.2990@gmail.com>
2022-12-04 01:45:10 -0600

    day 1 part 2

--- advent_of_code/day_1.py
+++ advent_of_code/day_1.py
@@ -1,12 +1,17 @@
-highest = 0
+highest = [0, 0, 0]
 current = 0
 with open("day_1.txt", "r") as calories:
     for calorie in calories:
         if calorie != "\n":
             current += int(calorie.strip())
         else:
-            if current > highest:
-                highest = current
+            if current > highest[0]:
+                highest[0] = current
+            elif current > highest[1]:
+                highest[1] = current
+            elif current > highest[2]:
+                highest[2] = current
             current = 0
 
-print(highest)
+print("part 1", highest[0])
+print("part 2", highest[0] + highest[1] + highest[2])
