74a1ff43fb7ebea4553eac93ff0bff7809f82c7f
Odyhibit <josh.2990@gmail.com>
2023-02-27 18:57:24 -0600

    using pillow to "hide" information in the Most significant bit.

--- pillow_test.py
+++ pillow_test.py
@@ -6,25 +6,57 @@ def hide_bit(original: int, bit: int) -> int:
     hidden_bit = bit << 7
     return striped + hidden_bit
 
+def stego():
+    with Image.open("worn_sm.png") as img, open("final_exam.png", "rb") as hidden:
+        px = img.load()
+        b_message = ""
+        while hex_of_byte := hidden.read(1).hex():
+            if hex_of_byte:
+                b_message += bin(int(hex_of_byte, 16))[2:].zfill(8)
 
-with Image.open("worn_sm.png") as img, open("final_exam.png", "rb") as hidden:
-    px = img.load()
-    data = hidden.read()
-    b_message = "".join([format(int(data[i]), "08b") for i in data])
-    width, height = img.width, img.height
+        print("STEGO")
+        print(b_message[0:64])
+        width, height = img.width, img.height
 
-    for y in range(height):
-        for x in range(width):
-            if len(b_message) > 0:
-                bit_to_hide = b_message[0:1]
-                image_byte = px[x, y]
-                px[x, y] = hide_bit(image_byte, int(bit_to_hide))
-                b_message = b_message[1:]
-                #print(hide_bit(image_byte, int(bit_to_hide)), image_byte, bit_to_hide)
-    img.save("test.png")
+        for y in range(height):
+            for x in range(width):
+                if len(b_message) > 0:
+                    bit_to_hide = b_message[0:1]
+                    image_byte = px[x, y]
+                    px[x, y] = hide_bit(image_byte, int(bit_to_hide))
+                    b_message = b_message[1:]
+        img.save("test.png")
 
-    orgnl = px[100, 100]
-    print(type(orgnl))
-    print(format(orgnl, "08b"))
-    print(format(hide_bit(orgnl, int("0")), "08b"))
-    print(format(hide_bit(orgnl, 1), "08b"))
+
+def unhide_byte(eight_bytes: [int]) -> int:
+    byte_string = ""
+    for byte in eight_bytes:
+        high_bit = byte & 0b10000000
+        bit = high_bit >> 7
+        byte_string += str(bit)
+    return int(byte_string,2)
+
+
+def unstego():
+    with Image.open("test.png") as img, open("output.png", "wb") as output:
+        raw_bytes = bytearray()
+        px = img.load()
+        width, height = img.width, img.height
+        data = []
+        for y in range(height):
+            for x in range(width):
+                data.append(px[x, y])
+        for i in range(0, len(data), 8):
+            raw_bytes.append(unhide_byte(data[i:i+8]))
+        print(hex(unhide_byte(data[0:8])), hex(unhide_byte(data[8:16])))
+        output.write(raw_bytes)
+
+
+test_byte = [0b10000000, 0b01000000, 0b10000000, 0b00010000, 0b10000000, 0b01000000, 0b10000000, 0b00000000]
+print(unhide_byte(test_byte))
+print(test_byte)
+print(test_byte[0:8])
+print(hide_bit(0xff, 0))
+print(hide_bit(0xff, 1))
+stego()
+unstego()
