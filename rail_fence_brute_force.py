3243412f57535b9dc4c4bf1f9529472355c4f23b
Odyhibit <josh.2990@gmail.com>
2023-06-06 23:39:51 -0500

    Brute force rail fence, with rainbow display added

--- rail_fence_brute_force.py
+++ rail_fence_brute_force.py
@@ -1,24 +1,27 @@
-def decode(cipher_str, rails):
-    cipher = list(cipher_str.replace(" ", "_"))
+red = "\u001b[31m"
+orange = "\u001b[38:5:208m"
+yellow = "\u001b[38:5:227m"
+green = "\u001b[32m"
+blue = "\u001b[34m"
+indigo = "\u001b[38:5:93m"
+violet = "\u001b[38:5:54m"
+reset = "\u001b[0m"
+rainbow = [red, orange, yellow, green, blue, indigo, violet]
+
+
+def decode(cipher_str: str, rails: int, offset: int = 0):
+    cipher = cipher_str.replace(" ", "_")
     period: int = 2 * (rails - 1)
-    index = 0
+    index = offset
     rail_fence = [[] for _ in range(rails)]
     for row in range(rails):
         for i in range(len(cipher)):
-            if i % period == row:
-                rail_fence[row].append(cipher[index])
-                index += 1
-            elif period - (i % period) == row:
+            if i % period == row or period - (i % period) == row:
                 rail_fence[row].append(cipher[index])
                 index += 1
             else:
                 rail_fence[row].append(" ")
-    return visual_to_text(rail_fence)
-
-
-def print_rail_fence(fence):
-    for i, rail in enumerate(fence):
-        print(rail)
+    return visual_to_text(rail_fence).replace("_", " "), rail_fence
 
 
 def visual_to_text(fence):
@@ -30,7 +33,58 @@ def visual_to_text(fence):
     return out
 
 
-print(decode("Tnex g hec xetsjn rtitenat iuiuoesn am t sesfsg ei pehttbe tnssla di", 6).replace("_"," "))
+def load_word_list(path: str) -> []:
+    with open(path, "r") as common_words:
+        common_word_list = []
+        while line := common_words.readline():
+            common_word_list.append(line.strip())
+        return common_word_list
 
-print(int(True), int(False))
-print(14//6, 14 % 6)
+
+def list_to_string(character_list: list) -> str:
+    return "".join(character_list)
+
+
+def display_rainbow_rails(fence: []):
+    for i, rail in enumerate(fence):
+        print(rainbow[i], list_to_string(rail), reset)
+
+
+def display_rainbow_plaintext(fence: []):
+    print(" ", end="")
+    for col in range(len(fence[0])):
+        for row in range(len(fence)):
+            print(rainbow[row], end="")
+            if fence[row][col] == "_":
+                print(" ", end="")
+            elif fence[row][col] != " ":
+                print(fence[row][col], end="")
+    print(reset)
+
+
+cipher = "Tnex g hec xetsjn rtitenat iuiuoesn am t sesfsg ei pehttbe tnssla di"
+common_word_list = load_word_list("common_words_min_3_letters.txt")
+highest_word_count = 0
+row_candidate = 0
+offset_candidate = 0
+
+for key in range(2, len(cipher) // 2 + 4):
+    period = 2 * (key - 1)
+    for offset in range(period):
+        word_count = 0
+        candidate, rails = decode(cipher, key)
+        for word in common_word_list:
+            if word in candidate:
+                word_count += 1
+        if word_count > highest_word_count:
+            highest_word_count = word_count
+            row_candidate = key
+            offset_candidate = offset
+        #  print(f"if there are {i} rows the word count is {word_count}")
+
+print(f"Looks like the row count is most likely {row_candidate} with offset {offset_candidate}")
+plaintext, rails = decode(cipher, row_candidate, offset_candidate)
+
+display_rainbow_rails(rails)
+display_rainbow_plaintext(rails)
+
